961ae278c9702521cc6b15dd8831b48609e98e9f
Vimalbharath <vimalbharath21@gmail.com>
2024-08-29 22:03:05 +0530

    button not working

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,26 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index,setIndex]=useState(1);
+  const{name, job, image, text}=people[index];
+  return (<article className='review'>
+    <div className='img-container'>
+      <img src ={image} alt ={name} className='person-img'></img>
+      <span className='quote-icon'>
+          <FaQuoteRight />
+      </span>
+    </div>
+    <div>
+      <h3 className='author'>{name}</h3>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <FaChevronLeft className='prev-btn'/>
+      <FaChevronRight className='next-btn'/>
+    </div>
+    <button className='random-btn'>surprise me</button>
+
+
+  </article>);
 };
 
 export default Review;
