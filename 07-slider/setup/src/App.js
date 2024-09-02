fba39412fb13ced31e66534cdf858891ac5deac2
Vimalbharath <vimalbharath21@gmail.com>
2024-09-02 11:45:44 +0530

    first last index set need to set auto slide

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -4,7 +4,17 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const[people,setPeople]=useState(data);
-  const[index,setIndex]=useState(2);
+  const[index,setIndex]=useState(0);
+
+  useEffect(()=>{
+    const lastIndex=people.length -1;
+    if(index<0){
+      setIndex(lastIndex);
+    }
+    if(index>lastIndex){
+      setIndex(0);
+    }
+  },[index,people])
 
   return (
   <section className='section'>
@@ -19,7 +29,8 @@ function App() {
           if(personIndex===index){
             position='activeSlide';
           }
-          if(personIndex===index-1){
+          if(personIndex===index-1 || (index===0 &&  personIndex===people.length-1) 
+            ){
             position='lastSlide';
           }
           return(<article className={position} key={id}>
