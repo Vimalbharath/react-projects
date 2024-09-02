c17c7ead29c288abdbe525c2e39ea34db8bbf29b
Vimalbharath <vimalbharath21@gmail.com>
2024-09-02 23:59:47 +0530

    excdept alert

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,25 +4,43 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  const [color,setColor]=useState([]);
+  const [color,setColor]=useState('');
   const [error,setError]=useState(false);
+  const [list,setList]=useState([]);
 
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log('gello');
+    console.log('hello');
+    try{
+      let colors = new Values(color).all(10)
+      setList(colors);
+      setError(false);
+      console.log(colors);
+
+    }
+    catch(error){
+      setError(true);
+      console.log(error);
+    }
   }
-  return (
+  return (<>
     <section className='container'>
       <h3>Enter the value:</h3>
-      <form>
-        <input></input>
-        <button className='btn' onClick={handleSubmit}>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={color}placeholder='#f15025' onChange={(e)=>setColor(e.target.value)}
+        className={`${error ?'error': null}`}></input>
+        <button className='btn' >Submit</button>
       </form> 
-      <section>
-      
     </section>
+     <section className='colors'>
+      {
+        list.map((solocolor,index)=>{
+          return <SingleColor key={index} {...solocolor} index={index} hex={solocolor.hex}></SingleColor>
+        })
+      }
+       
     </section>
-    
+    </>
   )
 }
 
