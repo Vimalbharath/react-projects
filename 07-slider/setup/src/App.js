5aed404acee8886f0a101b665a4f139af348220e
Vimalbharath <vimalbharath21@gmail.com>
2024-09-02 11:32:34 +0530

    need to fix end of array

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -4,7 +4,7 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const[people,setPeople]=useState(data);
-  const[index,setIndex]=useState(0);
+  const[index,setIndex]=useState(2);
 
   return (
   <section className='section'>
@@ -19,6 +19,9 @@ function App() {
           if(personIndex===index){
             position='activeSlide';
           }
+          if(personIndex===index-1){
+            position='lastSlide';
+          }
           return(<article className={position} key={id}>
             <img src={image} alt={name} className='person-img'/>
             <h4>{name}</h4>
@@ -28,10 +31,10 @@ function App() {
           </article>);
         })
       }
-      <button className='prev'>
+      <button className='prev' onClick={()=>setIndex(index-1)}>
         <FiChevronLeft/>
       </button>
-      <button className='next'>
+      <button className='next' onClick={()=>setIndex(index+1)}>
         <FiChevronRight/>
       </button>
 
