93f96d4f46c1921774b691539ccab39daf478efe
Vimalbharath <vimalbharath21@gmail.com>
2024-09-02 15:37:40 +0530

    form first try

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -4,7 +4,15 @@ function App() {
   
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>tired of being lorem ipsum ?</h3>
+    <form className='lorem-form'>
+      <label htmlFor='label' >
+        Paragraph
+        <input type='number'></input>
+      </label>
+    </form>
+  </section>
     )
 }
 
