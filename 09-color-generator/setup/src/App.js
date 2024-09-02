4d76fcb6f97f687d7fc371cd7a9f22657077147d
Vimalbharath <vimalbharath21@gmail.com>
2024-09-02 20:55:17 +0530

    my form

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,26 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor]=useState([]);
+  const [error,setError]=useState(false);
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log('gello');
+  }
+  return (
+    <section className='container'>
+      <h3>Enter the value:</h3>
+      <form>
+        <input></input>
+        <button className='btn' onClick={handleSubmit}>Submit</button>
+      </form> 
+      <section>
+      
+    </section>
+    </section>
+    
+  )
 }
 
 export default App
