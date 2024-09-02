c257a13b910ae9ef77314916df1290dbcaacbe43
Vimalbharath <vimalbharath21@gmail.com>
2024-09-03 00:15:40 +0530

    completed

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,9 +5,15 @@ const SingleColor = ({rgb,weight,index,hex}) => {
   const[alert,setAlert]=useState(false);
   const bcg=rgb.join(',');
   const hexValue=`#${hex}`;
-  return <article className={`color ${index>10&&'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}> 
+  useEffect(()=>{
+    const timeout=setTimeout(()=>{setAlert(false)},3000)
+  },[alert])
+  return <article className={`color ${index>10&&'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}
+  onClick={()=>{setAlert(true)
+  navigator.clipboard.writeText(hexValue)}}> 
   <p className='percent-value'>{weight}%</p>
   <p className='color-value'>{hexValue}</p>
+  {alert &&<p className='alert'> copied to clipboard</p>}
   </article>
 }
 
