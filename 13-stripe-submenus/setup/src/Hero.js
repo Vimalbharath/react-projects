2cf2ec07dfb329f4ab7cf23a62b24002d59ff4df
Vimalbharath <vimalbharath21@gmail.com>
2024-09-05 16:58:17 +0530

    done with hero

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -3,9 +3,25 @@ import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
-  const data=useGlobalContext();
-  console.log(data)
-  return <h2>hero component</h2>
+  const {closeSubmenu}=useGlobalContext();
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Financial infrastructure to grow your revenue
+          </h1>
+          <p>
+            Join the millions of companies of all sizes that use 
+            Stripe to accept payments online and in person, embed financial services, power custom revenue models, and build a more profitable business.
+          </p>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone'></img>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
