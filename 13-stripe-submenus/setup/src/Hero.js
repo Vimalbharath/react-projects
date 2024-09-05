1ed4a7085b3deeacc47d01e6d06c4198b0b70420
Vimalbharath <vimalbharath21@gmail.com>
2024-09-05 23:10:55 +0530

    sidebar complete

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -9,12 +9,13 @@ const Hero = () => {
       <div className='hero-center'>
         <article className='hero-info'>
           <h1>
-            Financial infrastructure to grow your revenue
+            Financial infrastructure to grow
           </h1>
           <p>
             Join the millions of companies of all sizes that use 
             Stripe to accept payments online and in person, embed financial services, power custom revenue models, and build a more profitable business.
           </p>
+          <button className='btn'>Start Now</button>
         </article>
         <article className='hero-images'>
           <img src={phoneImg} className='phone-img' alt='phone'></img>
