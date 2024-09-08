f2c642e0de4d2b371e1568f8e8ed78bbb7ede6d9
Vimalbharath <vimalbharath21@gmail.com>
2024-09-08 23:02:47 +0530

    navbar completed

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -4,9 +4,23 @@ import logo from '../logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className='navbar'>
+      <div className='nav-center'>
+      <Link>
+      <img className='logo' src={logo} alt='nav-bar-logo'/>
+      </Link>
+      <ul className='nav-links'>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+      </div>
+
+        
+    </nav>
   )
 }
 
