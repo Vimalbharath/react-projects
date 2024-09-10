b912889f1424d20acef1a482ace22c6cc327773c
Vimalbharath <vimalbharath21@gmail.com>
2024-09-10 08:12:39 +0530

    completed 15 projects

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -16,7 +16,27 @@ const SingleCocktail = () => {
         console.log(data)
         setLoading(false)
         if(data.drinks){
-
+            const{strDrink:name,
+            strDrinkThumb:image,
+            strAlcoholic:info,
+            strCategory:category,
+            strGlass:glass,
+            strInstructions:instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          }=data.drinks[0]
+          const ingredients=[strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5]
+            const newCocktail={
+              name,image,info,glass,category,instructions,ingredients
+            }
+            setCocktail(newCocktail)
         }else{
           setCocktail(null)
         }
@@ -27,10 +47,47 @@ const SingleCocktail = () => {
     }
     getCocktail()
   },[id])
+  if(loading){
+    return <Loading/>
+  }
+  if(!cocktail){
+    return <section className='section-title'>no cocktail to display</section>
+  }
+  const {name,image,info,glass,category,instructions,ingredients}=cocktail
   return (
-    <div>
-    <h2>{id} </h2>
-    </div>
+    <section className='section cocktail-section'>
+      <Link to="/" className='btn btn=primary'>back home</Link>
+      <h2 className='section-title'>{name}</h2>
+      <div className='drink'>
+        <img src={image} alt={name}/>
+        <div className='drink-info'>
+        <p>
+          <span className='drink-data'>name:</span>
+           {name}
+           </p>
+           <p>
+              <span className='drink-data'>category :</span> {category}
+            </p>
+            <p>
+              <span className='drink-data'>info :</span> {info}
+            </p>
+            <p>
+              <span className='drink-data'>glass :</span> {glass}
+            </p>
+            <p>
+              <span className='drink-data'>instructons :</span> {instructions}
+            </p>
+             <p>
+              <span className='drink-data'>ingredients :</span>
+              {ingredients.map((item, index) => {
+                return item ? <span key={index}> {item}</span> : null
+              })}
+            </p>
+      </div>
+      </div>
+      
+      
+      </section>
   )
 }
 
